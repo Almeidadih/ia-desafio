a6afbfaed41e12b2140a465649f97466150fccef
diego <almeidadieo488@gmail.com>
2026-08-19 21:17:51 -0300

    Primneiro Commit

---
+++ src/main/java/dio/iadesafio/IaDesafioApplication.java
@@ -0,0 +1,13 @@
+package dio.iadesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaDesafioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dio/iadesafio/IaDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.iadesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
